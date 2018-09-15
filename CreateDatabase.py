0bc84fc04a9e59d6c2b3839735f96655e28883e9
Philipp Kleppmann <philipp@kleppmann.de>
2018-09-15 11:11:36 +0200

    remove duplicates from dataset

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -8,7 +8,7 @@ cursor.execute('use food;')
 cursor.execute('create table environmental_cost (food_type varchar(50) not null, kg_co2_per_kg decimal(7,3) not null, '
                'primary key (food_type));')
 
-with open('data.csv', 'rt') as dataFile:
+with open('extensivedata.csv', 'rt') as dataFile:
     csvReader = csv.reader(dataFile, delimiter=',', quotechar='"')
     for row in csvReader:
         key = row[0]
