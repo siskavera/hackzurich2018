fb379708e08e1ed2d7f07dc9d1e7af49282a1db0
Philipp Kleppmann <philipp@kleppmann.de>
2018-09-15 14:00:39 +0200

    make js database connector use callbacks

---
+++ DatabaseConnector.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql');
+
+class DatabaseConnector {
+  static get(key, callback) {
+    var connection = mysql.createConnection({
+      host: "172.17.0.3",
+      user: "root",
+      password: "123",
+      database: "food",
+      insecureAuth: true
+    });
+
+    connection.connect(function(err) {
+      if (err) throw err;
+      });
+
+    connection.query('select * from environmental_cost where food_type="' + key + '";', function (err, result, fields) {
+      if (err) throw err;
+      callback(result[0].kg_co2_per_kg);
+      });
+
+    connection.end();
+  }
+}
---
+++ DatabaseConnectorTest.js
@@ -0,0 +1,3 @@
+DatabaseConnector.get("Peas", function (kg_co2_per_kg) {
+    console.log("result: " + kg_co2_per_kg);
+});
